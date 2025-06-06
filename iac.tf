8038fe0e0d7a61edf37c7f9d3ac5a37f3d0c9671
matthewyuh246 <matthewyuh246@gmail.com>
2025-06-06 13:05:44 +0900

    EC2 + VPC + Subnet

--- iac.tf
+++ iac.tf
@@ -1,11 +1,40 @@
 # Configure the AWS Provider
-provider "" {
-  region = ""
-  access_key = ""
-  secret_key = ""
+provider "aws" {
+  region = "ap-northeast-1"
+  profile = "admin"
 }
 
-resource "<provider>_<resource_type>" "name" {
-  key1 = "value1"
-  key2 = "value2"
+# EC2インスタンス
+resource "aws_instance" "iac-instance" {
+  ami = "ami-027fff96cc515f7bc"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "iac-instance"
+  }
 }
+
+# VPC
+resource "aws_vpc" "iac-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+    tags = {
+    Name = "iac-vpc"
+  }
+}
+
+# Subnet
+resource "aws_subnet" "iac-subnet" {
+  vpc_id     = aws_vpc.iac-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "iac-subnet"
+  }
+}
+
+# ひな型
+# resource "<provider>_<resource_type>" "name" {
+#   key1 = "value1"
+#   key2 = "value2"
+# }
